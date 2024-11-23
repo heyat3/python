e0fce7188e7a3588e86f5eec692c91c9e8956703
heyat3 <heyatdrone123@gmail.com>
2024-11-22 21:52:57 -0600

    adding 2nd file

--- television.py
+++ television.py
@@ -1,21 +1,33 @@
 class Television:
+    '''
+    A class representing the functions of a television
+    '''
     MIN_VOLUME = 0
     MAX_VOLUME = 2
     MIN_CHANNEL = 0
     MAX_CHANNEL = 3
 
+    '''
+    constructor method to initialize instance variables
+    '''
     def __init__(self):
         self.__status = False
         self.__muted = False
         self.__volume = self.MIN_VOLUME
         self.__channel = self.MIN_CHANNEL
 
+    '''
+    power method changes status variable to true or false depending if television is on or off
+    '''
     def power(self):
         if self.__status:
             self.__status = False
         else:
             self.__status = True
 
+    '''
+    mure method changes muted variable to true or false depending if television is muted
+    '''
     def mute(self):
         if self.__muted:
             self.__muted = False
@@ -23,6 +35,10 @@ class Television:
             self.__muted = True
             self.__volume = 0
 
+    '''
+    channel up and down change channel variable up or down
+    set channel to minimum or maximum respectively if over maximum or minimum
+    '''
     def channel_up(self):
         if self.__status:
             if self.__channel < self.MAX_CHANNEL:
@@ -37,6 +53,10 @@ class Television:
             else:
                 self.__channel = self.MAX_CHANNEL
 
+    '''
+        volume up and down change volume variable up or down
+        set volume to maximum or minimum respectively if over maximum or minimum
+    '''
     def volume_up(self):
         self.__muted = False
         if self.__status:
@@ -53,5 +73,8 @@ class Television:
             else:
                 self.__volume = self.MIN_VOLUME
 
+    '''
+    :return: Power = [status], Channel = [channel], Valume = [volume]
+    '''
     def __str__(self):
         return f'Power = {self.__status}, Channel = {self.__channel}, Volume = {self.__volume}'
---
+++ test_television.py
@@ -0,0 +1,27 @@
+import unittest
+from television import *
+
+class MyTestCase(unittest.TestCase):
+    def test__init__(self):
+        self.assertEqual(True, False)  # add assertion here
+
+    def test_power(self):
+
+
+    def test_mute(self):
+
+
+    def test_channel_up(self):
+
+
+    def test_channel_down(self):
+
+
+    def test_volume_up(self):
+
+
+    def test_volume_down(self):
+
+
+if __name__ == '__main__':
+    unittest.main()
