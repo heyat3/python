871fd9d3fbb23d46fbb1c3510d8558eba325667b
heyat3 <heyatdrone123@gmail.com>
2024-11-22 21:56:13 -0600

    updating files

--- test_television.py
+++ test_television.py
@@ -6,22 +6,22 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(True, False)  # add assertion here
 
     def test_power(self):
-
+        pass
 
     def test_mute(self):
-
+        pass
 
     def test_channel_up(self):
-
+        pass
 
     def test_channel_down(self):
-
+        pass
 
     def test_volume_up(self):
-
+        pass
 
     def test_volume_down(self):
-
+        pass
 
 if __name__ == '__main__':
     unittest.main()
